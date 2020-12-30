3d4deb05e20c9238fc3e236612935ecdf18bb193
[Joshua Constante] <[joshuaconstante2197@hotmail.com]>
2020-12-29 22:24:57 -0500

    Add razor pages project, display all account headers

---
+++ Collections/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileProcessingLibrary;
+using FileProcessingLibrary.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Collections.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public DisplayDbData GetDbData = new DisplayDbData();
+        public List<AccountHeader> Accounts;
+        
+        //public IndexModel(DisplayDbData getDbData)
+        //{
+        //    this.GetDbData = getDbData;
+        //}
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public void OnGet()
+        {
+            Accounts = GetDbData.DisplayAllAccounts();
+
+        }
+    }
+}
---
+++ FileProcessingLibrary/Services/DisplayDbData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace FileProcessingLibrary.Services
+{
+    public class DisplayDbData
+    {
+        public List<AccountHeader> DisplayAllAccounts()
+        {
+            var accounts = new List<AccountHeader>();
+            try
+            {
+                using (SqlConnection sqlCon = new SqlConnection(Config.ConnString))
+                {
+                    sqlCon.Open();
+                    var sql = "SELECT * FROM AccountHeader";
+                    using (SqlCommand cmd = new SqlCommand(sql, sqlCon))
+                    {
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                var accountHeader = new AccountHeader();
+
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    var fieldName = reader.GetName(i);
+                                    var property = accountHeader.GetType().GetProperty(fieldName);
+
+                                    if (property != null && !reader.IsDBNull(i))
+                                    {
+                                        property.SetValue(accountHeader, reader.GetValue(i), null);
+                                    }
+                                }
+                                accounts.Add(accountHeader);
+                            }
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var err = new CreateLogFiles();
+                err.ErrorLog(Config.DataPath + "err.log",":" + ex);
+                throw;
+            }
+            
+            return accounts;
+        }
+    }
+}
