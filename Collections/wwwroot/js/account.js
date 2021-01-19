4553a73691768daa165e80b04a5c32cfd216e9e8
Joshua Constante <joshuaconstante2197@hotmail.com>
2021-01-19 15:30:22 -0500

    Add option to switch between last comment and new comment

---
+++ Collections/wwwroot/js/account.js
@@ -0,0 +1,16 @@
+﻿$(".addCommentButton").click(function () {
+    var index = $(".addCommentButton").index(this);
+    $(".commentForm:eq(" + index + ")").css("display", "block");
+    $(".seeLastCommentButton:eq(" + index + ")").css("display", "block");
+    $(".lastComment:eq(" + index + ")").css("display", "none");
+    $(".addCommentButton:eq(" + index + ")").css("display", "none");
+})
+
+$(".seeLastCommentButton").click(function () {
+    var index = $(".seeLastCommentButton").index(this);
+    $(".commentForm:eq(" + index + ")").css("display", "none");
+    $(".seeLastCommentButton:eq(" + index + ")").css("display", "none");
+    $(".lastComment:eq(" + index + ")").css("display", "block");
+    $(".addCommentButton:eq(" + index + ")").css("display", "block");
+})
+
